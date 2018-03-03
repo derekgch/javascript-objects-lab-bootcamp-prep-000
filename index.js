c3073a2b7a221e4571878c8728a5bce5d720ecd9
derek chen <derek.g.chen@gmail.com>
2018-03-03 16:31:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  return Object.assign(object, {[key]:value})
 
 }
 //updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object:
