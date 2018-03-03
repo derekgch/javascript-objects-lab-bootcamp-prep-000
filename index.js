65a7b54940b791b4ab9445198de62c4c8dfce446
derek chen <derek.g.chen@gmail.com>
2018-03-03 16:28:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+  
+} 
+//updates `object` with the given `key` and `value` (it is destructive) and returns the entire updated object:
+
+function deleteFromObjectByKey(object, key) {
+
+}
+//deletes `key` from a clone of object and returns the new object
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+}
+//returns object without the delete key/value pair:
